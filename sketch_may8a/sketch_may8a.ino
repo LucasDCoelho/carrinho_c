0d695273d4bd7e70002caf9ba3a72f6e8b5efd4b
ALUNO C23 <alunoc23@unifor.local>
2025-05-13 09:33:31 -0300

    teste

--- sketch_may8a/sketch_may8a.ino
+++ sketch_may8a/sketch_may8a.ino
@@ -6,7 +6,8 @@ int bluetoothRx = 8;  // Pino RX do módulo Bluetooth conectado ao TX do Arduino
 
 // Inicialização do objeto para comunicação Bluetooth
 SoftwareSerial bluetooth(bluetoothRx, bluetoothTx);
-
+char ultimoComando = 0;
+char item = 0;
 
 void setup() 
 {
@@ -27,50 +28,69 @@ void setup()
 void loop() 
 {
     if (bluetooth.available() > 0) {
-    char item = bluetooth.read();
+    item = bluetooth.read();
     Serial.println(item);
-    if (item == 'w') {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
-    }
-    if (item == 'a') {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      analogWrite(6, 150);
-      digitalWrite(7, LOW);
-    }
-    if (item == 's') {
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-    }
-    if (item == 'd') {
-      digitalWrite(4, HIGH);
-      analogWrite(5, 150);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-    }
-    if(item == 'p'){
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-    }
-    if(item == '0'){
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-    }
-    if(item == '1'){
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-    }
-    delay(10);
+  
+      
+     switch(item){
+      case 'w': irParaFrente(); break;
+      case 's': irParaTras(); break;
+      case 'd': direita(); break;
+      case 'a': esquerda(); break;
+      case 'p': pare(); break;
+      case '0': zeroEsquerda(); break;
+      case '1': zeroDireita(); break;
+     }
+
+    
   }
+}
+
+void irParaFrente(){
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+}
+
+void irParaTras(){
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+}
+
+void direita(){
+  digitalWrite(4, HIGH);
+  analogWrite(5, 40);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+}
+
+void esquerda(){
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  analogWrite(6, 40);
+  digitalWrite(7, HIGH);
+}
+
+void pare(){
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+}
+
+void zeroEsquerda(){
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+}
+
+void zeroDireita(){
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
 }
