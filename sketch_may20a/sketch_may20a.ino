07e446db620898202b8fb6445f0f16fec0058044
LucasDCoelho <darkalp99@gmail.com>
2025-05-22 09:17:57 -0300

    add: melhorando codigo

--- sketch_may20a/sketch_may20a.ino
+++ sketch_may20a/sketch_may20a.ino
@@ -1,5 +1,18 @@
+#include <PID_v1.h>
+
 // C++ code
 //
+
+// Parâmetros PID
+double entrada, saida, setpoint = 0;
+double Kp = 1.5, Ki = 0.0, Kd = 4.5;
+
+// Instância do PID
+PID pid(&entrada, &saida, &setpoint, Kp, Ki, Kd, DIRECT);
+
+// Velocidade base do carrinho
+const int VELOCIDADE_BASE = 120;  
+
 void setup() {
   pinMode(A0, INPUT);
   pinMode(A5, INPUT);
@@ -10,7 +23,11 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
 
+  pid.SetMode(AUTOMATIC);
+  pid.SetOutputLimits(-100, 100);
+
   Serial.begin(9600);
+  
 }
 
 void loop() {
@@ -20,25 +37,34 @@ void loop() {
   Serial.println(sensorDireito);
   Serial.println(sensorEsquerdo);
 
+  entrada = sensorEsquerdo - sensorDireito;
+
+  // Calcula o PID
+  pid.Compute();
+
+  // Aplica a saída para ajustar velocidade
+  int velD = constrain(VELOCIDADE_BASE - saida, 0, 255);
+  int velE = constrain(VELOCIDADE_BASE + saida, 0, 255);
+
 
   if (sensorDireito < 100 && sensorEsquerdo < 100) {
-    irParaFrente();
+    irParaFrente(velD, velE);
   } else if (sensorDireito > 100 && sensorEsquerdo < 100) {
-    direita();
+    direita(velD, velE);
   } else if (sensorDireito < 100 && sensorEsquerdo > 100) {
-    esquerda();
+    esquerda(velD, velE);
   } else {
     pare();
   }
 
-  delay(10);  // Delay a little bit to improve simulation performance
+  delay(5);  // Delay a little bit to improve simulation performance
 }
 
 
-void irParaFrente() {
+void irParaFrente(int velEsquerda, int velDireita) {
   digitalWrite(4, HIGH);
-  analogWrite(5, 40);
-  analogWrite(6, 110);
+  analogWrite(5, velEsquerda);
+  analogWrite(6, velDireita);
   digitalWrite(7, LOW);
 }
 
@@ -49,17 +75,17 @@ void irParaTras() {
   digitalWrite(7, HIGH);
 }
 
-void direita() {
+void direita(int velDireita, int velEsquerda) {
   digitalWrite(4, HIGH);
-  analogWrite(5, 40);
-  digitalWrite(6, HIGH);
+  analogWrite(5, velEsquerda);
+  analogWrite(6, HIGH);
   digitalWrite(7, HIGH);
 }
 
-void esquerda() {
+void esquerda(int velDireita, int velEsquerda) {
   digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  analogWrite(6, 110);
+  analogWrite(5, LOW);
+  analogWrite(6, velDireita);
   digitalWrite(7, LOW);
 }
 
